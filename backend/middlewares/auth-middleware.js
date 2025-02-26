a99409f03628a8a456422214b6defa337aee2dd3
Mykola Kutsko <nomoremikolo2@gmail.com>
2025-02-26 18:32:36 +0200

    Added auth middleware

---
+++ backend/middlewares/auth-middleware.js
@@ -0,0 +1,19 @@
+const ApiError = require("../exeptions/api-errors");
+const tokenService = require("../service/token-service");
+module.exports = function (req, res, next) {
+  try {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) return next(ApiError.UnauthorizedError());
+
+    const accessToken = authHeader.split(" ")[1];
+    if (!accessToken) return next(ApiError.UnauthorizedError());
+
+    const userData = tokenService.validateAccessToken(accessToken);
+    if (!userData) return next(ApiError.UnauthorizedError());
+
+    req.user = userData;
+    next();
+  } catch (e) {
+    next(ApiError.UnauthorizedError());
+  }
+};
