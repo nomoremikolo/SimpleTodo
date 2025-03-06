3409e292a3942bc1fd7c6b845a9d5b6a2ee7bd09
Mykola Kutsko <nomoremikolo2@gmail.com>
2025-03-06 03:05:13 +0200

    Fixed create user api error

--- backend/services/user-service.js
+++ backend/services/user-service.js
@@ -1,8 +1,9 @@
+const ApiError = require("../exeptions/api-errors");
 const User = require("../models/user-model");
 
 exports.createUser = async ({ email, password }) => {
   const existingUser = await User.findOne({ email });
-  if (existingUser) throw new Error("Email already in use");
+  if (existingUser) throw ApiError.BadRequest("Email already in use");
 
   const user = new User({ email, password });
   await user.save();
